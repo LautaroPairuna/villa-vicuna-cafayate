160f8d9c5527157240cadbf177e56b590273ccb6
LautaroPairuna <lautaropairuna@gmail.com>
2025-04-30 17:54:59 -0300

    Eliminar la importación del componente Image en Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+// import Image from "next/image";
 import React, { memo } from "react";
 
 const Hero = memo(() => {
