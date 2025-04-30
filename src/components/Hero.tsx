4fefc7dc7964699e8f4ac9d13248a8462fbc1ccf
LautaroPairuna <lautaropairuna@gmail.com>
2025-04-30 17:57:21 -0300

    Eliminar la importación de la función de traducción en el componente Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 // import Image from "next/image";
 import React, { memo } from "react";
 
