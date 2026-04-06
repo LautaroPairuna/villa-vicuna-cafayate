1d373e0c8420f8d1f7ba7c828090db99f594c8ce
LautaroPairuna <lautaropairuna@gmail.com>
2026-04-05 22:15:39 -0300

    Conf Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config = {
   theme: {
     extend: {
       colors: {
@@ -16,3 +11,5 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+export default config;
